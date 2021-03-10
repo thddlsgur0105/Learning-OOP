84920b1926a902e1de5ce003e18627115d796e3d
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-03-10 14:28:09 +0900

    Started learning OOP

---
+++ object_loop.js
@@ -0,0 +1,13 @@
+var memberArray = ['egoing', 'graphittie', 'leezche'];
+
+var i = 0;
+while(i < memberArray.length) {
+    console.log(memberArray[i]);
+    i = i + 1;
+}
+
+var memberObject = {
+    manager: 'egoing',
+    developer: 'graphittie',
+    designer: 'leezche'
+};
