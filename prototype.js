1b07b70700597e9b4223c8b27f46564879b4cf29
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-03-10 23:16:16 +0900

    Started learning Prototype

--- prototype.js
+++ prototype.js
@@ -11,6 +11,11 @@ function Person(name, first, second, third) {
 }
 
 var kim = new Person("kim", 10, 20, 30);
+
+kim.sum = function() {
+    return `modified : ${this.first + this.second + this.third}`;
+}
+
 var lee = new Person("lee", 10, 10, 10);
 
 console.log("kim.sum()", kim.sum());
