7c83feacd5c5a61b98a78b7749d4029039fda84a
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-06-29 18:33:22 +0900

    Updated Promise

--- promise.js
+++ promise.js
@@ -1,17 +1,15 @@
-const amISexy = new Promise((resolve, reject) => {
-    resolve(2);
-});
-
-const timesTwo = number => number * 2;
-
-amISexy
-.then(timesTwo)
-.then(timesTwo)
-.then(timesTwo)
-.then(timesTwo)
-.then(timesTwo)
-.then(() => {
-    throw Error("Something is wrong")
+const p1 = new Promise((resolve) => {
+    setTimeout(resolve, 5000, "First");
 })
-.then(lastNumber => console.log(lastNumber))
-.catch(error => console.log(error));
+
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(reject, 1000, "This is rejected!");
+})
+
+const p3 = new Promise((resolve) => {
+    setTimeout(resolve, 3000, "Third");
+})
+
+const motherPromise = Promise.all([p1, p2, p3]);
+
+motherPromise.then(values => console.log(values)).catch(error => console.log(error));
