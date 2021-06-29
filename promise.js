caf40169a9e1c7e2e629035f947ebdf3b407647c
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-06-29 17:52:48 +0900

    Updated Promise

--- promise.js
+++ promise.js
@@ -1,9 +1,17 @@
 const amISexy = new Promise((resolve, reject) => {
-    // console Yes You Are ~== API returned value
-    setTimeout(resolve, 2000, "Yes You Are")
-    setTimeout(reject, 3000, "You Are Ugly")
+    resolve(2);
 });
 
+const timesTwo = number => number * 2;
+
 amISexy
-    .then(result => console.log(result))
-    .catch(error => console.log(error))
+.then(timesTwo)
+.then(timesTwo)
+.then(timesTwo)
+.then(timesTwo)
+.then(timesTwo)
+.then(() => {
+    throw Error("Something is wrong")
+})
+.then(lastNumber => console.log(lastNumber))
+.catch(error => console.log(error));
