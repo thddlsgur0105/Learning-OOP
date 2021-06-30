b22119f41239427584ac4562bf72f6c8b055e5ee
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-06-30 14:26:06 +0900

    Updated Promise

--- promise.js
+++ promise.js
@@ -1,18 +1,7 @@
-function work(callback) {
-    setTimeout(() => {
-        const start = Date.now();
-        for (let i = 0; i < 1000000000; i++) {
-            // loop
-        }
-        const end = Date.now();
-        console.log(`${end-start} ms`);
-        callback(end-start);
-    }, 0)
-}
-
-console.log(`작업 시작!`);
-work(ms => {
-    console.log(`작업이 끝났어요!`);
-    console.log(`${ms} ms 이 걸렸어요!`);
+const amISexy = new Promise((resolve, reject) => {
+    throw Error("something is wrong");
 });
-console.log(`작업 종료!`);
+
+amISexy
+    .then(value => console.log(value))
+    .catch(value => console.log(value));
